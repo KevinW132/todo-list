93108b7c0f73ed000bda36d58d2c7ca7b0021d7e
KevinW132 <kevinenriquedmo@gmail.com>
2025-01-10 14:32:29 -0600

    Terminado la lista de que hacer

--- src/app/componets/card/card.component.ts
+++ src/app/componets/card/card.component.ts
@@ -10,13 +10,26 @@ import { FormsModule } from '@angular/forms';
 })
 export class CardComponent {
   task:String = "";
+  isActive:boolean = true;
   lista: String[] = ["primero", "segundo"];
+  terminadas: String[] = ["tercero"];
   
   eliminar(posi: number){
     this.lista.splice(posi, 1);
   }
+  eliminarter(posi: number){
+    this.terminadas.splice(posi, 1);
+    if(this.terminadas.length == 0){
+      this.isActive = false
+    }
+  }
   agregar(){
     this.lista.push(this.task);
     this.task = "";
+  }
+  completadas(posi: number){
+    this.terminadas.push(this.lista[posi]);
+    this.lista.splice(posi, 1);
+    this.isActive = true
   }
 }
